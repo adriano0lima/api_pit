33502283e090dd1545ec4f6692d6ccb58b4fc46f
Adriano Lima <arlima810@gmail.com>
2024-11-11 20:22:51 -0300

    Primeiro envio

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+
+const index = require('./index')
+
+
+
+//Import Routes
+const carrinho = require("./routes/carrinho");
+const categoria = require("./routes/categoria");
+const perfil = require("./routes/perfil");
+const produto = require("./routes/produto");
+const usuario = require("./routes/usuario");
+
+
+
+
+//Set Routes
+app.use('/', carrinho);
+app.use('/', categoria);
+app.use('/', perfil);
+app.use('/', produto);
+app.use('/', usuario);
+
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
